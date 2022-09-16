ef220ff14f5a94b0c683ae6a5d57c55c7cfbb0a5
ajaychauhan4310 <95287200+ajaychauhan4310@users.noreply.github.com>
2022-09-16 17:17:06 +0530

    Add files via upload

---
+++ 02 Main Project 2 [GroceryApp]/app/src/main/java/com/ajaychauhan4310/groceryapp/GroceryRepository.kt
@@ -0,0 +1,8 @@
+package com.ajaychauhan4310.groceryapp
+
+class GroceryRepository(private val db:GroceryDatabase) {
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+
+    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
+}
---
+++ 02 Main Project 2 [GroceryApp]/app/src/main/java/com/ajaychauhan4310/groceryapp/GroceryViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.ajaychauhan4310.groceryapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class GroceryViewModelFactory(private val repository: GroceryRepository):ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return GroceryViewModel(repository) as T
+    }
+}
